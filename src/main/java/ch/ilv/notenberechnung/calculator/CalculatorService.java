a80f78adb226f55e1697284d0efe2436061b54c6
WS111 <flavio.fiechter@bbzbl-it.ch>
2023-04-27 15:25:15 +0200

    calculating subject and semester

--- src/main/java/ch/ilv/notenberechnung/calculator/CalculatorService.java
+++ src/main/java/ch/ilv/notenberechnung/calculator/CalculatorService.java
@@ -1,9 +1,23 @@
 package ch.ilv.notenberechnung.calculator;
 
+import ch.ilv.notenberechnung.grade.Grade;
+import ch.ilv.notenberechnung.grade.GradeRepository;
+import ch.ilv.notenberechnung.schoolsubject.SchoolSubject;
+import ch.ilv.notenberechnung.schoolsubject.SchoolSubjectRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
 
 @Service
 public class CalculatorService {
+    private final GradeRepository gradeRepository;
+    private final SchoolSubjectRepository schoolSubjectRepository;
+    public CalculatorService(GradeRepository gradeRepository, SchoolSubjectRepository schoolSubjectRepository) {
+        this.gradeRepository = gradeRepository;
+        this.schoolSubjectRepository = schoolSubjectRepository;
+    }
+
     public double calculate(Calculator calculator) {
         if (calculator.getMaxPoints() == 0) {
             if (calculator.getAchievedPoints() == 0) {
@@ -16,5 +30,36 @@ public class CalculatorService {
             }
         }
          return calculator.getAchievedPoints() / calculator.getMaxPoints() * 5 + 1;
+    }
+
+    public double AvgSubject(Long subjectid) {
+        List<Grade> grades = gradeRepository.findAll();
+        double gradesToAVG = 0.0;
+        int size = 0;
+        for (Grade grade : grades) {
+            if (Objects.equals(grade.getSchoolSubject().getId(), subjectid)) {
+                gradesToAVG += (grade.getGrade());
+                size += 1;
+            }
+        }
+        return gradesToAVG/size;
+    }
+
+    public double AvgSemester(Long semesterid) {
+        List<SchoolSubject> schoolSubjects = schoolSubjectRepository.findAll();
+        List<Grade> grades = gradeRepository.findAll();
+        double gradesToAVG = 0.0;
+        int size = 0;
+        for (SchoolSubject schoolSubject : schoolSubjects) {
+            if (Objects.equals(schoolSubject.getSemester().getId(), semesterid)) {
+                for (Grade grade : grades) {
+                    if (Objects.equals(grade.getSchoolSubject().getId(), schoolSubject.getId())) {
+                        gradesToAVG += (grade.getGrade());
+                        size += 1;
+                    }
+                }
+            }
+        }
+        return gradesToAVG/size;
     }
 }
